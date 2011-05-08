56ad7a4f129ba8448f0ac6466a9e4af43779a0a6
tylerc230 <tylerc230@gmail.com>
2011-05-08 11:15:49 -0700

    restructuring fs

---
+++ client/ios/DrinkAppTests/DrinkAppTests.h
@@ -0,0 +1,17 @@
+//
+//  DrinkAppTests.h
+//  DrinkAppTests
+//
+//  Created by Tyler Casselman on 5/8/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface DrinkAppTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ client/ios/DrinkAppTests/DrinkAppTests.m
@@ -0,0 +1,33 @@
+//
+//  DrinkAppTests.m
+//  DrinkAppTests
+//
+//  Created by Tyler Casselman on 5/8/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "DrinkAppTests.h"
+
+
+@implementation DrinkAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in DrinkAppTests");
+}
+
+@end
