4e521a19f01a9a28144e3af82709a7ca69f0a492
tylerc230 <tylerc230@gmail.com>
2011-05-16 21:47:13 -0700

    adding new rails server app

---
+++ server/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DrinkApp::Application.config.secret_token = 'f86bcd1a696577d35b92eafa416cc1c36ea1044871f3b515e905c5bd04de28320816c2126f89c73a1d900c34847e584566d8254101b43a03d3d692b2a2e16f48'
