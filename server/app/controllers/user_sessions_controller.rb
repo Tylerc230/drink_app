8fb7fe31c8232c22e33d64e3de5e01f8486f5c80
tylerc230 <tylerc230@gmail.com>
2011-05-19 23:49:38 -0700

    server calling fb successfully

--- server/app/controllers/user_sessions_controller.rb
+++ server/app/controllers/user_sessions_controller.rb
@@ -1,8 +1,24 @@
+require 'net/http'
+require 'net/https'
 class UserSessionsController < ApplicationController
   def create()
   	token = params[:token]
   	user_session = UserSession.new(:token => token)
 	user_session.save
+	call_facebook(token)
+
+  end
+  def call_facebook(token)
+	url = 'graph.facebook.com'
+	url_encode_token = CGI::escape(token)
+	query = '&sdk=ios&sdk_version=2&format=json'
+	path = '/me?access_token='+url_encode_token + query
+
+	puts 'url' + url
+	puts 'path ' + path
+	http = Net::HTTP.new(url, 443)
+	http.use_ssl = true
+	resp, data = http.get2(path, {'User-Agent' => 'FacebookConnect'})
   end
 
   def destroy
