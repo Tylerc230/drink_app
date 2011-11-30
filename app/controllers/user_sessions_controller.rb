ca7a24a82d301ec07d9ad98336a51a19584205be
Tyler Casselman <tcasselman@Tylers-MacBook-Pro.local>
2011-11-29 22:59:55 -0800

    adding user session test
    I created a test for when the token is bad. We want to retun a 404
    in that case.

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -6,9 +6,15 @@ class UserSessionsController < ApplicationController
 	def create
 		token = params[:token]
 		data = {}
-    data['me'] = User.get_user_for_token(token)
-    data['friends'] = User.get_friends_for_token(token)
-    data['drinks'] = Drink.all
-		render :text => data.to_json
+    status = 200
+    begin
+      data['me'] = User.get_user_for_token(token)
+      data['friends'] = User.get_friends_for_token(token)
+      data['drinks'] = Drink.all
+    rescue RuntimeError => re
+      data[:error] = re.message
+      status = 404
+    end
+		render :text => data.to_json, :status => status
 	end
 end
--- test/functional/user_sessions_controller_test.rb
+++ test/functional/user_sessions_controller_test.rb
@@ -9,4 +9,9 @@ class UserSessionsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should fail with bad token" do
+    post :create, :token => "0TskHp8ckMfJz93YUNP1_W-_Mseso2kAiH-UqmnZL8I.eyJpdiI6InF6RXRiMWdrSV9uOHJlY0EyTTUxWlEifQ.sPpxvWZov3Y6XmO9Si1P99z0mi9oJeYwrQvYOvaxvU5GBNPHcfDGjFstUHeYrqtBHx9Nvj633XT5S0f8ymfgfwMUuc2BkShvaT_oeEvfo99EfUOb9FWJGCa2upNzBL42KuM06jDF7lKixTWJwzlXrg"
+    assert_response :missing
+  end
+
 end
