636cb1a1c8eba6f8ff8c5be376609298a11e1ed2
Tyler Casselman <tylerc230@gmail.com>
2012-01-04 22:13:07 -0800

    Adding and deleting reward conditions from reward page
    you can now edit rewards and reward conditions from the same page

--- app/admin/reward_conditions.rb
+++ app/admin/reward_conditions.rb
@@ -1,9 +1,9 @@
 ActiveAdmin.register RewardCondition do
   menu false
-  member_action :delete do
-    reward_condition = RewardCondition.find(params[:id])
-    reward_condition.delete
-    redirect_to :action => :show
-  end
+  #member_action :delete do
+  #  reward_condition = RewardCondition.find(params[:id])
+  #  reward_condition.delete
+  #  redirect_to :back
+  #end
 
 end
