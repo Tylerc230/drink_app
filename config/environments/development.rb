7d8db83a1e50aae37cb9e15715d954b3fc716076
Tyler Casselman <tylerc230@gmail.com>
2012-01-22 15:34:10 -0800

    finally got devise mailer working
    I skipped a step in the setup, also adding actual mail sending in
    dev env (prob want to change that later)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,6 +18,8 @@ DrinkApp::Application.configure do
   #config.action_mailer.raise_delivery_errors = false
   # added by tyler for active admin support
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :sendmail #:smtp
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
