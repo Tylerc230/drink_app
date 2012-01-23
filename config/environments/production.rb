2a030c60e26312fde2bfc48d4f68b86ef68d4610
Tyler Casselman <tylerc230@gmail.com>
2012-01-23 09:10:09 -0800

    configuring production to use gmail to send mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,6 +37,15 @@ DrinkApp::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { :host => 'gentle-journey-907.heroku.com/:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'gmail.com',
+    :user_name            => ENV['GMAIL_USERNAME'],
+    :password             => ENV['GMAIL_PASSWORD'],
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  }
 
   # Enable threaded mode
   # config.threadsafe!
