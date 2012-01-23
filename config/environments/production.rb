7e82462bd32f13b1defc007b4e2bf2c01448f4a2
Tyler Casselman <tylerc230@gmail.com>
2012-01-23 09:21:31 -0800

    updating url in email
    the url in the email from devise has to point back to our heroku
    app instance

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,7 @@ DrinkApp::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => 'gentle-journey-907.heroku.com/:3000' }
+  config.action_mailer.default_url_options = { :host => 'gentle-journey-907.heroku.com/' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
